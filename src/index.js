d6662b90ad4f17decf3b5aaad9efbff9a02a0b33
Coco <corriestoddard@gmail.com>
2025-08-29 16:29:35 -0700

    Update

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Hello, Node!"));
-
-app.listen(3000, () => console.log("Server running on 3000"));
---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello, Node!"));
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
