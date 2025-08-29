bf81a379d42c493e58e188c537cae479a02801fd
Coco <corriestoddard@gmail.com>
2025-08-29 16:25:40 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, Node!"));
+
+app.listen(3000, () => console.log("Server running on 3000"));
